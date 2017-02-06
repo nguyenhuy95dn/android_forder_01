5f7642b12b199abe5e48b2aa2e364312ce6ca8a5
Nguyen Minh Duong <nguyen.minh.duong@framgia.com>
2017-02-06 10:14:21 +0700

    init project

---
+++ app/src/main/java/com/example/duong/android_forder_01/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.duong.android_forder_01.ui.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.duong.android_forder_01.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
