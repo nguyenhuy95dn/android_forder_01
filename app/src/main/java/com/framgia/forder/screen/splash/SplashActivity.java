1aec1e566a7ee65792b6964b1c0e496111c7cbb6
huy <nguyenhuy95dn@gmail.com>
2017-04-17 09:51:55 +0700

    feature fix bug choose domain

--- app/src/main/java/com/framgia/forder/screen/splash/SplashActivity.java
+++ app/src/main/java/com/framgia/forder/screen/splash/SplashActivity.java
@@ -4,7 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import com.framgia.forder.data.source.DomainRepository;
+import com.framgia.forder.data.source.UserRepository;
+import com.framgia.forder.data.source.local.DomainLocalDataSource;
+import com.framgia.forder.data.source.local.UserLocalDataSource;
+import com.framgia.forder.data.source.local.sharedprf.SharedPrefsApi;
+import com.framgia.forder.data.source.local.sharedprf.SharedPrefsImpl;
+import com.framgia.forder.screen.chooseDomain.ChooseDomainActivity;
 import com.framgia.forder.screen.login.LoginActivity;
+import com.framgia.forder.screen.main.MainActivity;
 import com.framgia.forder.utils.navigator.Navigator;
 
 public class SplashActivity extends AppCompatActivity {
@@ -15,11 +23,23 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPrefsApi prefsApi = new SharedPrefsImpl(getApplicationContext());
+        final DomainRepository domainRepository =
+                new DomainRepository(null, new DomainLocalDataSource(prefsApi, null));
+        final UserRepository userRepository =
+                new UserRepository(null, new UserLocalDataSource(prefsApi));
         mHandler = new Handler();
         mRunnable = new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent intent;
+                if (userRepository.getUser() != null && domainRepository.getDomainId() == 0) {
+                    intent = new Intent(SplashActivity.this, ChooseDomainActivity.class);
+                } else if (domainRepository.getDomainId() != 0) {
+                    intent = new Intent(SplashActivity.this, MainActivity.class);
+                } else {
+                    intent = new Intent(SplashActivity.this, LoginActivity.class);
+                }
                 new Navigator(SplashActivity.this).startActivity(intent);
                 finish();
             }
