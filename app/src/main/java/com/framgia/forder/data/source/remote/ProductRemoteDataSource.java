99377101796378161d3307eef97dbd1c96e1ab7e
tantuan127 <levutantuan@Les-MacBook-Pro.local>
2017-04-19 08:25:25 +0700

    [Sprint 2] Feature Logic Product List

--- app/src/main/java/com/framgia/forder/data/source/remote/ProductRemoteDataSource.java
+++ app/src/main/java/com/framgia/forder/data/source/remote/ProductRemoteDataSource.java
@@ -14,8 +14,6 @@ import rx.functions.Func1;
 
 public class ProductRemoteDataSource extends BaseRemoteDataSource
         implements ProductDataSource.RemoteDataSource {
-    private final int START_SUB_LIST = 0;
-    private final int END_SUB_LIST = 6;
 
     public ProductRemoteDataSource(FOrderApi FOrderApi) {
         super(FOrderApi);
@@ -31,15 +29,6 @@ public class ProductRemoteDataSource extends BaseRemoteDataSource
                             return Observable.just(productResponse.getListProduct());
                         }
                         return Observable.error(new NullPointerException());
-                    }
-                })
-                .map(new Func1<List<Product>, List<Product>>() {
-                    @Override
-                    public List<Product> call(List<Product> products) {
-                        if (products.size() <= END_SUB_LIST) {
-                            return products;
-                        }
-                        return products.subList(START_SUB_LIST, END_SUB_LIST);
                     }
                 });
     }
--- app/src/main/java/com/framgia/forder/screen/listProduct/ListProductContract.java
+++ app/src/main/java/com/framgia/forder/screen/listProduct/ListProductContract.java
@@ -1,7 +1,10 @@
 package com.framgia.forder.screen.listProduct;
 
+import com.framgia.forder.data.model.Product;
+import com.framgia.forder.data.source.remote.api.error.BaseException;
 import com.framgia.forder.screen.BasePresenter;
 import com.framgia.forder.screen.BaseViewModel;
+import java.util.List;
 
 /**
  * This specifies the contract between the view and the presenter.
@@ -11,11 +14,21 @@ interface ListProductContract {
      * View.
      */
     interface ViewModel extends BaseViewModel<Presenter> {
+        void onGetListAllProductError(BaseException exception);
+
+        void onGetListAllProductSuccess(List<Product> products);
+
+        void onAddToCartError(BaseException exception);
+
+        void onAddToCartSuccess();
     }
 
     /**
      * Presenter.
      */
     interface Presenter extends BasePresenter {
+        void addToCart(Product product);
+
+        void getListAllProduct();
     }
 }
