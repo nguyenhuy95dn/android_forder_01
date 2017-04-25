9fc58064802780474acc9e9f4481f7efe54843bf
nguyen.tien.vinh@framgia.com <nguyen.tien.vinh@framgia.com>
2017-04-25 11:44:55 +0700

    fake data notification

--- app/src/main/java/com/framgia/forder/data/source/NotificationRepository.java
+++ app/src/main/java/com/framgia/forder/data/source/NotificationRepository.java
@@ -1,7 +1,10 @@
 package com.framgia.forder.data.source;
 
+import com.framgia.forder.data.model.CollectionAvatar;
+import com.framgia.forder.data.model.Image;
 import com.framgia.forder.data.model.Notification;
 import com.framgia.forder.data.source.remote.NotificationRemoteDataSource;
+import java.util.ArrayList;
 import java.util.List;
 import rx.Observable;
 
@@ -16,7 +19,17 @@ public class NotificationRepository {
         mRemoteDataSource = remoteDataSource;
     }
 
-    public Observable<List<Notification>> getListNotificationI() {
-        return mRemoteDataSource.getListNotification();
+    public Observable<List<Notification>> getListNotification() {
+        //TODO: Fake data, remove later
+        List<Notification> notifications = new ArrayList<>();
+        String title = "Bạn vừa hủy đơn hàng của shop Nếp Cẩm";
+        String time = "Khoảng 30 phút trước";
+        String url = "http://www.papawestray.co.uk/images/shop-interior.jpg";
+        for (int i = 0; i < 10; i++) {
+            Notification notification =
+                    new Notification(title, time, new CollectionAvatar(new Image(url)));
+            notifications.add(notification);
+        }
+        return Observable.just(notifications);
     }
 }
