8fd0dfded1b74635fc89b68358070a09c53a5a07
= <=>
2017-05-10 10:48:11 +0700

    [Sprint 4] Feature Join Domain Shop Management

--- app/src/main/java/com/framgia/forder/screen/shopmanagement/ItemShopInfoViewModel.java
+++ app/src/main/java/com/framgia/forder/screen/shopmanagement/ItemShopInfoViewModel.java
@@ -1,20 +1,28 @@
 package com.framgia.forder.screen.shopmanagement;
 
-import android.content.Context;
+import android.databinding.ObservableInt;
 import com.framgia.forder.R;
+import com.framgia.forder.data.model.Domain;
 import com.framgia.forder.data.model.ShopInfo;
+import com.framgia.forder.data.model.ShopManagement;
 
 /**
  * Created by levutantuan on 5/8/17.
  */
 
 public class ItemShopInfoViewModel {
-    private final Context mContext;
     private final ShopInfo mShopInfo;
+    private final Domain mDomain;
+    private final ObservableInt mButtonJoinDomain;
+    private final ObservableInt mTextButton;
+    private final ObservableInt mTextStatus;
 
-    public ItemShopInfoViewModel(Context context, ShopInfo shopInfo) {
-        this.mContext = context;
-        mShopInfo = shopInfo;
+    ItemShopInfoViewModel(ShopManagement shopManagement, int position) {
+        mShopInfo = shopManagement.getShopInfos().get(position);
+        mDomain = shopManagement.getShopDomains().get(position);
+        mButtonJoinDomain = new ObservableInt();
+        mTextButton = new ObservableInt();
+        mTextStatus = new ObservableInt();
     }
 
     public String getDomainId() {
@@ -33,30 +41,76 @@ public class ItemShopInfoViewModel {
 
     public String getNumberUser() {
         if (mShopInfo != null) {
-            return mContext.getString(R.string.user_with_count, mShopInfo.getNumberUser());
+            return String.valueOf(mShopInfo.getNumberUser());
         }
-        return mContext.getString(R.string.user_with_count, 0);
+        return String.valueOf(0);
     }
 
     public String getNumberShop() {
         if (mShopInfo != null) {
-            return mContext.getString(R.string.shop_with_count,
-                    Integer.parseInt(String.valueOf(mShopInfo.getNumberShop())));
+            return String.valueOf(mShopInfo.getNumberShop());
         }
-        return mContext.getString(R.string.shop_with_count, 0);
+        return String.valueOf(0);
     }
 
     public String getNumberProduct() {
         if (mShopInfo != null) {
-            return mContext.getString(R.string.product_with_count, mShopInfo.getNumberProduct());
+            return String.valueOf(mShopInfo.getNumberProduct());
         }
-        return mContext.getString(R.string.product_with_count, 0);
+        return String.valueOf(0);
     }
 
     public String getStatus() {
-        if (mShopInfo != null && mShopInfo.getDomain() != null) {
-            return mContext.getString(R.string.status_domain, mShopInfo.getDomain().getStatus());
+        if (mDomain != null) {
+
+            switch (mDomain.getStatus()) {
+                case NONE:
+                    onRequest();
+                    mTextStatus.set(R.color.transparent);
+                    break;
+                case PENDING:
+                    onCancel();
+                    mTextStatus.set(R.drawable.border_pedding);
+                    break;
+                case APPROVED:
+                    onCancel();
+                    mTextStatus.set(R.drawable.border_status);
+                    break;
+                default:
+                    break;
+            }
+            return String.valueOf(mDomain.getStatus());
         }
-        return mContext.getString(R.string.status_domain, "");
+        return "";
+    }
+
+    public void onRequestCancelJoinDomainClick() {
+        if (mButtonJoinDomain.get() == R.drawable.button_blue) {
+            onCancel();
+        } else {
+            onRequest();
+        }
+    }
+
+    private void onRequest() {
+        mButtonJoinDomain.set(R.drawable.button_blue);
+        mTextButton.set(R.string.request);
+    }
+
+    private void onCancel() {
+        mButtonJoinDomain.set(R.drawable.button_red);
+        mTextButton.set(R.string.cancel);
+    }
+
+    public ObservableInt getButtonJoinDomain() {
+        return mButtonJoinDomain;
+    }
+
+    public ObservableInt getTextButton() {
+        return mTextButton;
+    }
+
+    public ObservableInt getTextStatus() {
+        return mTextStatus;
     }
 }
