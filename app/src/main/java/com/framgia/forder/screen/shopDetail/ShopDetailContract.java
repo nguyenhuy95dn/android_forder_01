207af87c3836f4fa4c606176b5679f78000d6450
huy <nguyenhuy95dn@gmail.com>
2017-06-01 16:20:25 +0700

    Fix ui logic detail shop

--- app/src/main/java/com/framgia/forder/screen/shopDetail/ShopDetailContract.java
+++ app/src/main/java/com/framgia/forder/screen/shopDetail/ShopDetailContract.java
@@ -1,10 +1,7 @@
 package com.framgia.forder.screen.shopDetail;
 
-import com.framgia.forder.data.model.Shop;
-import com.framgia.forder.data.source.remote.api.error.BaseException;
 import com.framgia.forder.screen.BasePresenter;
 import com.framgia.forder.screen.BaseViewModel;
-import java.util.List;
 
 /**
  * This specifies the contract between the view and the presenter.
@@ -14,9 +11,6 @@ interface ShopDetailContract {
      * View.
      */
     interface ViewModel extends BaseViewModel<Presenter> {
-        void onGetListShopSuccess(List<Shop> shops);
-
-        void onGetListShopError(BaseException error);
     }
 
     /**
