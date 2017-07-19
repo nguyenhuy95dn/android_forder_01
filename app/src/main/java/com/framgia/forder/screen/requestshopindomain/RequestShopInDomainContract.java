b14684fee9e4a4b8d087d83249d6236d48983bb5
unknown <nguyenhuy95dn@gmail.com>
2017-07-19 13:16:23 +0700

    Feature Logic List Shop Request Next

--- app/src/main/java/com/framgia/forder/screen/requestshopindomain/RequestShopInDomainContract.java
+++ app/src/main/java/com/framgia/forder/screen/requestshopindomain/RequestShopInDomainContract.java
@@ -1,7 +1,10 @@
 package com.framgia.forder.screen.requestshopindomain;
 
+import com.framgia.forder.data.source.remote.api.error.BaseException;
+import com.framgia.forder.data.source.remote.api.response.ShopRequestResponse;
 import com.framgia.forder.screen.BasePresenter;
 import com.framgia.forder.screen.BaseViewModel;
+import java.util.List;
 
 /**
  * This specifies the contract between the view and the presenter.
@@ -11,11 +14,23 @@ interface RequestShopInDomainContract {
      * View.
      */
     interface ViewModel extends BaseViewModel<Presenter> {
+        void onGetListRequestShopSuccess(List<ShopRequestResponse.ShopContain> shops);
+
+        void onGetMessageError(BaseException error);
+
+        void onRequestShopSuccess();
+
+        void showProgressBarDialog();
+
+        void hideProgressBarDialog();
     }
 
     /**
      * Presenter.
      */
     interface Presenter extends BasePresenter {
+        void getListRequestShop(int domainId);
+
+        void requestToAcceptRejectShopToDomain(int domainId, int shopId, String status);
     }
 }
--- app/src/main/java/com/framgia/forder/screen/requestshopindomain/RequestShopInDomainViewModel.java
+++ app/src/main/java/com/framgia/forder/screen/requestshopindomain/RequestShopInDomainViewModel.java
@@ -1,19 +1,40 @@
 package com.framgia.forder.screen.requestshopindomain;
 
+import com.framgia.forder.data.model.DomainManagement;
+import com.framgia.forder.data.source.remote.api.error.BaseException;
+import com.framgia.forder.data.source.remote.api.response.ShopRequestResponse;
+import com.framgia.forder.utils.navigator.Navigator;
+import com.framgia.forder.widgets.dialog.DialogManager;
+import java.util.List;
+
 /**
  * Exposes the data to be used in the Requestshopindomain screen.
  */
 
-public class RequestShopInDomainViewModel implements RequestShopInDomainContract.ViewModel {
+public class RequestShopInDomainViewModel implements RequestShopInDomainContract.ViewModel,
+        RequestShopInDomainAdapter.RequestShopInDomainListener {
+    private static final String STATUS_APPROVED = "approved";
+    private static final String STATUS_REJECTED = "rejected";
 
     private RequestShopInDomainContract.Presenter mPresenter;
+    private final Navigator mNavigator;
+    private final RequestShopInDomainAdapter mAdapter;
+    private final DomainManagement mDomainManagement;
+    private final DialogManager mDialogManager;
 
-    public RequestShopInDomainViewModel() {
+    RequestShopInDomainViewModel(Navigator navigator, RequestShopInDomainAdapter adapter,
+            DomainManagement domainManagement, DialogManager dialogManager) {
+        mNavigator = navigator;
+        mAdapter = adapter;
+        mDomainManagement = domainManagement;
+        mDialogManager = dialogManager;
+        mAdapter.setListener(this);
     }
 
     @Override
     public void onStart() {
         mPresenter.onStart();
+        mPresenter.getListRequestShop(mDomainManagement.getId());
     }
 
     @Override
@@ -24,5 +45,46 @@ public class RequestShopInDomainViewModel implements RequestShopInDomainContract
     @Override
     public void setPresenter(RequestShopInDomainContract.Presenter presenter) {
         mPresenter = presenter;
+    }
+
+    @Override
+    public void onGetListRequestShopSuccess(List<ShopRequestResponse.ShopContain> shops) {
+        mAdapter.updateData(shops);
+    }
+
+    @Override
+    public void onGetMessageError(BaseException error) {
+        mNavigator.showToastCustom(error.getMessage());
+    }
+
+    @Override
+    public void onRequestShopSuccess() {
+        mPresenter.getListRequestShop(mDomainManagement.getId());
+    }
+
+    @Override
+    public void showProgressBarDialog() {
+        mDialogManager.showProgressDialog();
+    }
+
+    @Override
+    public void hideProgressBarDialog() {
+        mDialogManager.dismissProgressDialog();
+    }
+
+    public RequestShopInDomainAdapter getAdapter() {
+        return mAdapter;
+    }
+
+    @Override
+    public void onClickAcceptShop(ShopRequestResponse.ShopContain shopContain) {
+        mPresenter.requestToAcceptRejectShopToDomain(mDomainManagement.getId(),
+                shopContain.getShopId(), STATUS_APPROVED);
+    }
+
+    @Override
+    public void onClickRejectShop(ShopRequestResponse.ShopContain shopContain) {
+        mPresenter.requestToAcceptRejectShopToDomain(mDomainManagement.getId(),
+                shopContain.getShopId(), STATUS_REJECTED);
     }
 }
