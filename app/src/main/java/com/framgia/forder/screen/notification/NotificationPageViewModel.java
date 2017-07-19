f1e4baf420dd6be56c16039857317dfe7e3e6689
= <=>
2017-07-19 16:07:33 +0700

    [Sprint Maitain] Event ItemClick Notification

--- app/src/main/java/com/framgia/forder/screen/notification/NotificationPageViewModel.java
+++ app/src/main/java/com/framgia/forder/screen/notification/NotificationPageViewModel.java
@@ -1,13 +1,9 @@
 package com.framgia.forder.screen.notification;
 
 import android.databinding.BaseObservable;
-import android.databinding.Bindable;
-import android.graphics.Color;
-import com.framgia.forder.BR;
 import com.framgia.forder.data.model.Notification;
 import com.framgia.forder.data.source.remote.api.error.BaseException;
 import com.framgia.forder.screen.BaseRecyclerViewAdapter;
-import com.framgia.forder.utils.StatusCode;
 import com.framgia.forder.utils.navigator.Navigator;
 import java.util.List;
 
@@ -20,24 +16,14 @@ public class NotificationPageViewModel extends BaseObservable
         BaseRecyclerViewAdapter.OnRecyclerViewItemClickListener<Object> {
 
     private NotificationPageContract.Presenter mPresenter;
-    private NotificationPageAdapter mNotificationPageAdapter;
-    private Navigator mNavigator;
-    private Notification mNotification;
-    private int mColorBackgroundNotification;
-    private BaseRecyclerViewAdapter.OnRecyclerViewItemClickListener<Object> mItemClickListener;
+    private final NotificationPageAdapter mNotificationPageAdapter;
+    private final Navigator mNavigator;
 
     NotificationPageViewModel(NotificationPageAdapter notificationPageAdapter,
             Navigator navigator) {
         mNotificationPageAdapter = notificationPageAdapter;
         mNavigator = navigator;
         mNotificationPageAdapter.setItemClickListener(this);
-    }
-
-    NotificationPageViewModel(Notification notification,
-            BaseRecyclerViewAdapter.OnRecyclerViewItemClickListener<Object> itemClickListener) {
-        mNotification = notification;
-        mItemClickListener = itemClickListener;
-        initColorNotification();
     }
 
     @Override
@@ -67,49 +53,10 @@ public class NotificationPageViewModel extends BaseObservable
 
     @Override
     public void onItemRecyclerViewClick(Object item) {
-        //TODO: Item click
-    }
 
-    public void onItemClicked() {
-        if (mItemClickListener == null) {
-            return;
-        }
-        mItemClickListener.onItemRecyclerViewClick(mNotification);
-    }
-
-    public String getNotificationImage() {
-        return "";
-    }
-
-    public String getNotificationTitle() {
-        return mNotification.getMessage();
-    }
-
-    public String getNotificationTime() {
-        return mNotification.getTimeNotificationFormat();
-    }
-
-    private void initColorNotification() {
-        if (mNotification != null) {
-            if (mNotification.isRead()) {
-                setColorBackgroundNotification(Color.WHITE);
-            } else {
-                setColorBackgroundNotification(Color.parseColor(StatusCode.COLOR_NOTIFICATION));
-            }
-        }
     }
 
     public NotificationPageAdapter getNotificationPageAdapter() {
         return mNotificationPageAdapter;
-    }
-
-    @Bindable
-    public int getColorBackgroundNotification() {
-        return mColorBackgroundNotification;
-    }
-
-    public void setColorBackgroundNotification(int colorBackgroundNotification) {
-        mColorBackgroundNotification = colorBackgroundNotification;
-        notifyPropertyChanged(BR.colorBackgroundNotification);
     }
 }
